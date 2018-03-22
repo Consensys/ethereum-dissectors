ef3e3b57e674ac3840bf26be118b4e0081aba0a2
wcgcyx <wcgcyx@gmail.com>
2018-03-22 13:53:51 +1000

    Build issue fixed

--- packet-ethereum.c
+++ packet-ethereum.c
@@ -1,29 +1,29 @@
-#include "config.c"
+#include "config.h"
 
 #include <epan/packet.h>
 
-#define ETH_PORT 30303
+#define ETHEREUM_PORT 30303
 
-static int proto_eth = -1;
+static int proto_ethereum = -1;
 
-static int dissect_eth(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_) {
-    col_set_str(pinfo->cinfo, COL_PROTOCOL, "ETH");
+static int dissect_ethereum(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_) {
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, "ETHEREUM");
     col_clear(pinfo->cinfo, COL_INFO);
 
     return tvb_captured_length(tvb);
 }
 
-void proto_register_eth(void) {
-    proto_eth = proto_register_protocol (
+void proto_register_ethereum(void) {
+    proto_ethereum = proto_register_protocol (
 		    "Ethereum Protocol",
-		    "ETH",
-		    "eth"
+		    "ETHEREUM",
+		    "ethereum"
 		    );
 }
 
-void proto_reg_handoff_eth(void) {
-    static dissector_handle_t eth_handle;
+void proto_reg_handoff_ethereum(void) {
+    static dissector_handle_t ethereum_handle;
 
-    eth_handle = create_dissector_handle(dissect_eth, proto_eth);
-    dissector_add_uint("udp.port", ETH_PORT, eth_handle);
+    ethereum_handle = create_dissector_handle(dissect_ethereum, proto_ethereum);
+    dissector_add_uint("udp.port", ETHEREUM_PORT, ethereum_handle);
 }
