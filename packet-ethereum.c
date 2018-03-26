e43dde4cd7c2e7e506b71aa2350428777d093054
wcgcyx <wcgcyx@gmail.com>
2018-03-26 15:56:15 +1000

    Basic Dissector

--- packet-ethereum.c
+++ packet-ethereum.c
@@ -5,20 +5,76 @@
 #define ETHEREUM_PORT 30303
 
 static int proto_ethereum = -1;
+static int hf_ethereum_pdu_hash = -1;
+static int hf_ethereum_pdu_signature = -1;
+static int hf_ethereum_pdu_type = -1;
+static int hf_ethereum_pdu_data = -1;
+static gint ett_ethereum = -1;
 
 static int dissect_ethereum(tvbuff_t *tvb, packet_info *pinfo, proto_tree *tree _U_, void *data _U_) {
-    col_set_str(pinfo->cinfo, COL_PROTOCOL, "ETHEREUM");
+    gint offset = 0;
+    col_set_str(pinfo->cinfo, COL_PROTOCOL, "ETHDEVP2PDISCO");
     col_clear(pinfo->cinfo, COL_INFO);
+
+    proto_item *ti = proto_tree_add_item(tree, proto_ethereum, tvb, 0, -1, ENC_NA);
+
+    proto_tree *ethereum_tree = proto_item_add_subtree(ti, ett_ethereum);
+    proto_tree_add_item(ethereum_tree, hf_ethereum_pdu_hash, tvb, offset, 32, ENC_BIG_ENDIAN);
+    offset += 32;
+    proto_tree_add_item(ethereum_tree, hf_ethereum_pdu_signature, tvb, offset, 65, ENC_BIG_ENDIAN);
+    offset += 65;
+    proto_tree_add_item(ethereum_tree, hf_ethereum_pdu_type, tvb, offset, 1, ENC_BIG_ENDIAN);
+    offset += 1;
+    proto_tree_add_item(ethereum_tree, hf_ethereum_pdu_data, tvb, offset, -1, ENC_BIG_ENDIAN);
 
     return tvb_captured_length(tvb);
 }
 
 void proto_register_ethereum(void) {
+
+    static hf_register_info hf[] = {
+	{ &hf_ethereum_pdu_hash,
+	    { "ETHEREUM DEVP2P Hash", "Ethdevp2p.hash",
+	    FT_BYTES, SEP_SPACE,
+	    NULL, 0X0,
+	    NULL, HFILL }
+	}, 
+
+	{ &hf_ethereum_pdu_signature,
+	    { "ETHEREUM DEVP2P Signature", "Ethdevp2p.signature",
+	    FT_BYTES, SEP_SPACE,
+	    NULL, 0X0,
+	    NULL, HFILL }
+	},
+
+	{ &hf_ethereum_pdu_type,
+	    { "ETHEREUM DEVP2P Type", "Ethdevp2p.type",
+	    FT_BYTES, SEP_SPACE,
+	    NULL, 0X0,
+	    NULL, HFILL }
+	}, 
+
+	{ &hf_ethereum_pdu_data,
+	    { "ETHEREUM DEVP2P Data", "Ethdevp2p.data",
+	    FT_BYTES, SEP_SPACE,
+	    NULL, 0X0,
+	    NULL, HFILL }
+	}
+    };
+
+    /* Setup protocol subtree array */
+    static gint *ett[] = {
+	&ett_ethereum
+    };
+
     proto_ethereum = proto_register_protocol (
-		    "Ethereum Protocol",
-		    "ETHEREUM",
-		    "ethereum"
+		    "Ethereum devp2p discovery protocol",
+		    "ETHDEVP2PDISCO",
+		    "ethdevp2pdisco"
 		    );
+
+    proto_register_field_array(proto_ethereum, hf, array_length(hf));
+    proto_register_subtree_array(ett, array_length(ett));    
 }
 
 void proto_reg_handoff_ethereum(void) {
