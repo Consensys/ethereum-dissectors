e3c18ef7484be916aa05c4b051a838940327c388
Raúl Kripalani <raul.kripalani@consensys.net>
2018-06-20 19:33:35 +0100

    Move test script to test subdirectory.

--- test/test.py
+++ test/test.py
@@ -1,42 +1,48 @@
+#!/usr/bin/env python
+
 import subprocess
 import json
 import unittest
 
-class EthereumDiscoTesting(unittest.TestCase):
+
+class EthereumDiscoveryTesting(unittest.TestCase):
+
     def setUp(self):
-	output = subprocess.check_output(["../wireshark-ninja/run/tshark", "-r", "./test.pcapng", "-T", "json"])
-    	self.js = json.loads(output)
-    
+        output = subprocess.check_output(["../../wireshark-ninja/run/tshark", "-r", "./test.pcapng", "-T", "json"])
+        self.pcap_output = json.loads(output)
+
     def test_ping(self):
-	Ping = 0
-        for i in self.js:
-	    try:
-		temp = (i["_source"]["layers"]["ethereum.disc"])
-	    except:
-		continue
-	    if (temp["ethereum.disc.packet"] == "PING"):
-		Ping += 1
-		try:
-		    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.ping.recipient.udp_port"]
-		    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.ping.sender.udp_port"]
-		    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.ping.expiration"]
-		    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.ping.version"]
-		    temp["ethereum.disc.packet_type"]
-		    temp["ethereum.disc.signature"]
-		    temp["ethereum.disc.hash"]
-		except:
-		    self.fail()
-	self.assertEqual(Ping, 967)
-    
+        ping_cnt = 0
+
+        for frame in self.pcap_output:
+                
+            try:
+                temp = (frame["_source"]["layers"]["ethereum.disc"])
+            except:
+                continue
+            if (temp["ethereum.disc.packet"] == "PING"):
+                ping_cnt += 1
+                try:
+                    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.ping.recipient.udp_port"]
+                    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.ping.sender.udp_port"]
+                    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.ping.expiration"]
+                    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.ping.version"]
+                    temp["ethereum.disc.packet_type"]
+                    temp["ethereum.disc.signature"]
+                    temp["ethereum.disc.hash"]
+                except:
+                    self.fail()
+        self.assertEqual(ping_cnt, 967)
+
     def test_pong(self):
-	Pong = 0
-	for i in self.js:
-	    try:
-		temp = (i["_source"]["layers"]["ethereum.disc"])
-	    except:
-		continue
-	    if (temp["ethereum.disc.packet"] == "PONG"):
-		Pong += 1
+        Pong = 0
+        for i in self.js:
+            try:
+                temp = (i["_source"]["layers"]["ethereum.disc"])
+            except:
+                continue
+            if (temp["ethereum.disc.packet"] == "PONG"):
+                Pong += 1
                 try:
                     temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.pong.recipient.udp_port"]
                     temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.pong.expiration"]
@@ -46,7 +52,7 @@ class EthereumDiscoTesting(unittest.TestCase):
                     temp["ethereum.disc.hash"]
                 except:
                     self.fail()
-	self.assertEqual(Pong, 403)
+        self.assertEqual(Pong, 403)
 
     def test_findNode(self):
         findNode = 0
@@ -57,26 +63,26 @@ class EthereumDiscoTesting(unittest.TestCase):
                 continue
             if (temp["ethereum.disc.packet"] == "FIND_NODE"):
                 findNode += 1
-	    	try:
-		    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.find_node.expiration"]
-		    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.find_node.target"]
-		    temp["ethereum.disc.packet_type"]
+                try:
+                    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.find_node.expiration"]
+                    temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.find_node.target"]
+                    temp["ethereum.disc.packet_type"]
                     temp["ethereum.disc.signature"]
                     temp["ethereum.disc.hash"]
-		except:
-		    self.fail()
+                except:
+                    self.fail()
         self.assertEqual(findNode, 80)
 
     def test_nodes(self):
-	nodes = 0
-	for i in self.js:
+        nodes = 0
+        for i in self.js:
             try:
                 temp = (i["_source"]["layers"]["ethereum.disc"])
             except:
                 continue
             if (temp["ethereum.disc.packet"] == "NODES"):
                 nodes += 1
-		try:
+                try:
                     temp["ethereum.disc.packet_tree"]["ethereum.disc.packet.nodes.expiration"]
                     temp["ethereum.disc.packet_type"]
                     temp["ethereum.disc.signature"]
@@ -86,17 +92,18 @@ class EthereumDiscoTesting(unittest.TestCase):
         self.assertEqual(nodes, 144)
 
     def test_error(self):
-	error = 0
-	for i in self.js:
+        error = 0
+        for i in self.js:
             try:
                 temp = (i["_source"]["layers"]["ethereum.disc"])
             except:
                 continue
             if (temp["ethereum.disc.packet"] != "PING" and
-			temp["ethereum.disc.packet"] != "PONG" and
-			temp["ethereum.disc.packet"] != "FIND_NODE" and
-			temp["ethereum.disc.packet"] != "NODES"):
+                        temp["ethereum.disc.packet"] != "PONG" and
+                        temp["ethereum.disc.packet"] != "FIND_NODE" and
+                        temp["ethereum.disc.packet"] != "NODES"):
                 error += 1
         self.assertEqual(error, 0)
+
 
 unittest.main()
